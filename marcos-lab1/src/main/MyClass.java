113eefba6e9cd7c6b906bf5a2b523719235a531b
User 1 <user1@test.com>
2014-03-13 23:22:37 -0500

    added MyClass

---
+++ marcos-lab1/src/main/MyClass.java
@@ -0,0 +1,13 @@
+package main;
+
+public class MyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("change 1");
+	}
+
+}
